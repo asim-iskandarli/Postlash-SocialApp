70b7a9d14faeaf1b2d9eb026fbed5db261cb4fb2
asim-iskandarli <asim.iskandarli@gmail.com>
2025-01-30 17:19:39 +0400

    first commit

--- client/src/components/home/Stories/index.tsx
+++ client/src/components/home/Stories/index.tsx
@@ -4,7 +4,7 @@ import StoryList from "./StoryList";
 
 const Stories = () => {
   return (
-    <div className="px-8 py-6 bg-white rounded-lg dark:bg-gray-800 shadow-sm dark:shadow-gray-800">
+    <div className="px-3 md:px-8 py-4 md:py-6 bg-white rounded-lg dark:bg-gray-800 shadow-sm dark:shadow-gray-800">
       <h2 className="mb-6 font-semibold text-gray-500 text-2xl dark:text-gray-300">
         Hekayələr
       </h2>
